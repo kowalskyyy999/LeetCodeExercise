39dc8ac5e56be3a4d89b306df8e3c8c95597bb5f
kowalskyyy999 <ngegemesin69@gmail.com>
2024-01-15 20:14:39 +0700

    fix palindrome

--- palindrome_number/src/main.rs
+++ palindrome_number/src/main.rs
@@ -2,10 +2,21 @@ struct Solution();
 
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        let x_string = x.to_string();
-        let first_char = x_string.chars().next().unwrap();
-        
-        &x_string.starts_with(first_char) == &x_string.ends_with(first_char)
+        match x {
+            i32::MIN..=-1 => false,
+            0..=i32::MAX => {
+                let x_string = x.to_string();
+                let mut temp = Vec::new();
+
+                for c in x_string.chars() {
+                    temp.push(c);
+                }
+                temp.reverse();
+
+                let palindrome = temp.into_iter().collect::<String>().parse::<i64>().unwrap();
+                palindrome == x as i64
+            }
+        }
     }
 }
 
