f849bbe8c9e4a492d197fd6aff2573c586ba4db4
kowalskyyy999 <rionugroho70@gmail.com>
2024-01-17 17:40:41 +0700

    fix substring

--- longest_substring_without_repeating_characters/src/main.rs
+++ longest_substring_without_repeating_characters/src/main.rs
@@ -1,18 +1,32 @@
+use std::collections::HashMap;
+
 struct Solution ();
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32{
-        let chars = s.chars();
-        let mut unique_char: Vec<char> = Vec::new();
-
-        for c in chars {
-            if unique_char.contains(&c) != true {
-                unique_char.push(c);
+        
+        match s.len() {
+            1 => {1},
+            _ => {
+                let mut start: i32 = 0;
+                let mut result: i32 = 0;
+                let mut seen: HashMap<char, usize> = std::collections::HashMap::new();
+        
+                let characters = s.chars();
+        
+                for (i, c) in characters.enumerate() {
+                    
+                    if seen.get(&c).map_or(0 as usize, |v| *v) >= start as usize {
+                        start = seen.get(&c).map_or(0 as i32, |v| *v as i32) + 1;
+                    }
+                    
+                    result = std::cmp::max(result, i as i32 - start + 1);
+                    seen.insert(c, i);
+                }
+        
+                result 
             }
         }
-
-        unique_char.len() as i32
-
     }
 }
 
@@ -20,8 +34,10 @@ fn main() {
     // println!("Hello, world!");
 
     let test_case1= String::from("abcabccbb");
-    let test_case2 = "bbbb";
-    let test_case3 = "pwwkew";
+    let test_case2 = String::from("bbbb");
+    let test_case3 = String::from("pwwkew");
+    let test_case4 = String::from(" ");
 
-    Solution::length_of_longest_substring(test_case1);
+    let result = Solution::length_of_longest_substring(test_case4);
+    println!("{result}");
 }
